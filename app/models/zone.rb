1f86cd9660d9fd8a78e8b2c4bce65ef364f58f64
Garrett Heinlen <heinleng@gmail.com>
2014-11-15 23:13:24 +1100

    Adding in a basic zone model (and sample yaml data to load)

---
+++ app/models/zone.rb
@@ -0,0 +1,21 @@
+class Zone < OpenStruct
+  DB = YAML.load_file(File.open("db/fixtures.yml"))
+
+  class << self
+    def all
+      DB["load_zones"].map do |zone_json|
+        new(zone_json[1])
+      end
+    end
+
+    def find(id)
+      yaml = DB["load_zones"].find { |yml_id, _data| yml_id.to_i == id.to_i }
+      raise "no yaml you idiot.. id: #{id}" unless yaml
+      new(yaml[1])
+    end
+  end
+
+  def initialize(data)
+    super(data)
+  end
+end
