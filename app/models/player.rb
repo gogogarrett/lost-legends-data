c1b7b52260bcb6ddb163a2e457a53d8675bab370
Garrett Heinlen <heinleng@gmail.com>
2014-11-16 13:22:49 +1100

    Hooking up the AR records.

--- app/models/player.rb
+++ app/models/player.rb
@@ -1,25 +1,18 @@
-class Player < OpenStruct
-  DB = YAML.load_file(File.open("db/fixtures.yml"))
+class Player < ActiveRecord::Base
+  has_many :slots
+  has_many :inventories
 
-  class << self
-    def all
-      DB["load_players"].map do |zone_json|
-        new(zone_json[1])
-      end
-    end
-
-    def find(id)
-      yaml = DB["load_players"].find { |yml_id, _data| yml_id.to_i == id.to_i }
-      raise "no yaml you idiot.. id: #{id}" unless yaml
-      new(yaml[1])
+  def items
+    inventories.map(&:item_id).map do |item_id|
+      Item.find(item_id)
     end
   end
 
-  def initialize(data)
-    super(data)
+
+  def equipped_items
+    slots.map(&:item_id).map do |item_id|
+      Item.find(item_id)
+    end
   end
 
-  def update(data)
-    puts "WE UPDATED THE USER"
-  end
 end
