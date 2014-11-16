25ca68e9c0c2dcf46e1a55d72602c1dda3174c2a
Garrett Heinlen <heinleng@gmail.com>
2014-11-16 15:17:39 +1100

    removing #items and using #inventories
    Inventories is the join between player/item

--- app/representers/player_representer.rb
+++ app/representers/player_representer.rb
@@ -9,6 +9,6 @@ module PlayerRepresenter
   property :hunts
   property :rubies
 
-  property :items, getter: -> (args) { represented.items.map(&:id) }
   property :slots, getter: -> (args) { represented.slots.map(&:id) }
+  property :inventories, getter: -> (args) { represented.inventories.map(&:id) }
 end
