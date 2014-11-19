fad155b1ff9059057f11bd44e77267d7e4b55a42
Garrett Heinlen <heinleng@gmail.com>
2014-11-19 14:40:39 +1100

    cleaning up routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,14 @@ Rails.application.routes.draw do
 
   namespace :api, defaults: {format: :json} do
     namespace :v1 do
-      resources :zones,         only: [:index, :show]
-      resources :battles,       only: [:show]
-      resources :players,       only: [:show, :update]
-      resources :items,         only: [:index, :show]
-      resources :slots,         only: [:index, :show, :create, :update, :destroy]
-      resources :inventories,   only: [:index, :show, :create, :update, :destroy]
+      resources :zones,             only: [:index, :show]
+      resources :battles,           only: [:show]
+      resources :player_battles,    only: [:show, :create, :update, :destroy]
+      resources :monsters,          only: [:show]
+      resources :players,           only: [:show, :update]
+      resources :items,             only: [:index, :show]
+      resources :slots,             only: [:index, :show, :create, :update, :destroy]
+      resources :inventories,       only: [:index, :show, :create, :update, :destroy]
     end
   end
 end
