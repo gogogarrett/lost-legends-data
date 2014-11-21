88f019120fff86338682d62c3297b1e20b4c156d
Garrett Heinlen <heinleng@gmail.com>
2014-11-21 12:07:53 +1100

    settling on how i want to structure the routes.. no #action attack.

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,10 @@
 Rails.application.routes.draw do
 
-  namespace :api, defaults: {format: :json} do
+  namespace :api, defaults: { format: :json } do
     namespace :v1 do
       resources :zones,             only: [:index, :show]
       resources :battles,           only: [:show]
-      resources :player_battles do#,    only: [:show, :create, :update, :destroy]
-        member do
-          post :attack
-        end
-      end
+      resources :player_battles,    only: [:show, :create, :update, :destroy]
       resources :monsters,          only: [:show]
       resources :players,           only: [:show, :update]
       resources :items,             only: [:index, :show]
